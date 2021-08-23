df720fdbd502550a38ef70d47f75b01b95e06123
Tony <tung.dinhthanh.work@gmail.com>
2021-08-22 23:39:20 -0700

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Instructions from './components/loginComponents/form'
+function App() {
+  return (
+    <Instructions/>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/loginComponents/form.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+ class Instructions extends Component {
+  render() {
+    return(
+      <p>Click on an emoji to view the emoji short name.</p>
+    )
+  }
+}
+
+export default Instructions;
